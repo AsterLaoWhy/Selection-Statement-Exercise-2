222796e30529a26d27977cf75f91f2e7fb781259
Jon <jonathon_rylatt@yahoo.com>
2023-12-07 14:49:09 +0800

    Added switch cases for classes

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please type your favorite class");
+            string favoriteClass = Console.ReadLine().ToLower();
+
+            switch (favoriteClass)
+            {
+                case "chemistry":
+                    Console.WriteLine("Walter White coming in");
+                    break;
+                case "math":
+                    Console.WriteLine("Do you celebrate Pi Day?");
+                    break;
+                case "gym":
+                    Console.WriteLine("Sam Sulek or The Rock?");
+                    break;
+                case "english":
+                    Console.WriteLine("A rose by any other name would smell just as sweet");
+                    break;
+                case "shop":
+                    Console.WriteLine("Let's make clocks and soupe up our cars");
+                    break;
+                default:
+                    Console.WriteLine("No idea what that is. How about you describe it to me. I promise I'll listen.");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
